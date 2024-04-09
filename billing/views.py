b66d212d323a76aa2f643dbe7d793e4e42442084
iamsamienda <samuelendale7373@gmail.com>
2024-04-09 19:54:47 +0300

    Add viewsets for Employee, Customer, Product, and Transaction models in billing app

--- billing/views.py
+++ billing/views.py
@@ -1,3 +1,24 @@
-from django.shortcuts import render
+from rest_framework.viewsets import ModelViewSet
 
-# Create your views here.
+from .serializers import EmployeeSerializer, CustomerSerializer, ProductSerializer, TransactionSerializer
+from .models import Employee, Customer, Product, Transaction
+
+
+class EmployeeViewSet(ModelViewSet):
+    queryset = Employee.objects.all()
+    serializer_class = EmployeeSerializer
+
+
+class CustomerViewSet(ModelViewSet):
+    queryset = Customer.objects.all()
+    serializer_class = CustomerSerializer
+
+
+class ProductViewSet(ModelViewSet):
+    queryset = Product.objects.all()
+    serializer_class = ProductSerializer
+
+
+class TransactionViewSet(ModelViewSet):
+    queryset = Transaction.objects.all()
+    serializer_class = TransactionSerializer
