515e9e244f291cb4aef7ec548cb063b113354efe
iamsamienda <samuelendale7373@gmail.com>
2024-04-09 21:09:04 +0300

    Refactor Employee and Customer models, serializers, and views in billing app

--- billing/models.py
+++ billing/models.py
@@ -5,17 +5,17 @@ from django.contrib.auth import get_user_model
 
 
 class Customer(models.Model):
-    first_name = models.CharField(max_length=50)
-    last_name = models.CharField(max_length=50)
-    total_billed_amount = models.DecimalField(
-        MinValueValidator(0.0), max_digits=10, decimal_places=2)
+    first_name = models.CharField(max_length=50, default='a')
+    last_name = models.CharField(max_length=50, default='a')
+    total_billed_amount = models.DecimalField(validators=[MinValueValidator(0.0)],
+                                              max_digits=10, decimal_places=2, default=0.0)
 
 
 
 class Product(models.Model):
     productname = models.CharField(max_length=50)
-    price = models.DecimalField(MinValueValidator(
-        1.0), decimal_places=2, max_digits=10)
+    price = models.DecimalField(validators=[MinValueValidator(
+        1.0)], decimal_places=2, max_digits=10)
     stock = models.PositiveBigIntegerField(default=0)
 
 
@@ -24,6 +24,6 @@ class Transaction(models.Model):
     product = models.ForeignKey(Product, on_delete=models.CASCADE)
     quantity = models.PositiveBigIntegerField(default=0)
     total_amount = models.DecimalField(
-        MinValueValidator(1.0), decimal_places=2, max_digits=10)
+        validators=[MinValueValidator(1.0)], decimal_places=2, max_digits=10)
     timestamp = models.DateTimeField(auto_now_add=True)
     employee = models.ForeignKey(get_user_model(), on_delete=models.CASCADE)
