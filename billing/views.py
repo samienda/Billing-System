47440d71d757cdbd23e0b837f1c7fce2b6ee14b6
iamsamienda <samuelendale7373@gmail.com>
2024-04-09 21:27:46 +0300

    Add DjangoFilterBackend and update permissions and filters in billing views.py

--- billing/views.py
+++ billing/views.py
@@ -1,5 +1,8 @@
-from rest_framework.viewsets import ModelViewSet
+from django_filters.rest_framework import DjangoFilterBackend
 
+from rest_framework.viewsets import ModelViewSet
+from rest_framework.permissions import IsAuthenticated
+from rest_framework.filters import SearchFilter, OrderingFilter
 from .serializers import CustomerSerializer, ProductSerializer, TransactionSerializer
 from .models import Customer, Product, Transaction
 
@@ -7,13 +10,31 @@ from .models import Customer, Product, Transaction
 class CustomerViewSet(ModelViewSet):
     queryset = Customer.objects.all()
     serializer_class = CustomerSerializer
+    filter_backends = [DjangoFilterBackend, SearchFilter, OrderingFilter]
+    permission_classes = [IsAuthenticated]
+    filterset_fields = '__all__'
+    OrderingFilter = '__all__'
+    search_fields = '__all__'
 
 
 class ProductViewSet(ModelViewSet):
     queryset = Product.objects.all()
     serializer_class = ProductSerializer
+    filter_backends = [DjangoFilterBackend, SearchFilter, OrderingFilter]
+    permission_classes = [IsAuthenticated]
+    filterset_fields = '__all__'
+    OrderingFilter = '__all__'
+    search_fields = '__all__'
 
 
 class TransactionViewSet(ModelViewSet):
     queryset = Transaction.objects.all()
     serializer_class = TransactionSerializer
+    filter_backends = [DjangoFilterBackend, SearchFilter, OrderingFilter]
+    permission_classes = [IsAuthenticated]
+    filterset_fields = '__all__'
+    OrderingFilter = '__all__'
+    search_fields = '__all__'
+
+    def perform_create(self, serializer):
+        serializer.save(employee=self.request.user)
